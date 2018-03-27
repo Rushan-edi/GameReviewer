576d44619de2b8a34df16cb397661a4578901125
Rushan <randika266@hotmail.com>
2018-03-27 23:07:48 +0530

    implemented main menu ui components

--- app/src/main/java/com/mobile/gamereviewer/MainActivity.java
+++ app/src/main/java/com/mobile/gamereviewer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mobile.gamereviewer;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar= (Toolbar)findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Game Reddit");
+        toolbar.setTitleTextColor(Color.parseColor("#8c5ca4"));
     }
 }
